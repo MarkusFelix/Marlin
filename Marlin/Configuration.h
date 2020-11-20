1a61cdae06be794f84af6448890d84fec83bb0c6
Markus Felix <markus.felix@gmail.com>
2020-11-20 16:19:21 +0100

    [MOD] for MPSM smaller X gantry and withou BLTouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -128,11 +128,11 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-#define MOTHERBOARD BOARD_MELZI_CREALITY
+#define MOTHERBOARD BOARD_MKS_SGEN_L
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "MPSM v2 1.1.5"
+#define CUSTOM_MACHINE_NAME "MPSM v2 MKS sGen L"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -1116,8 +1116,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 120
-#define Y_BED_SIZE 120
+#define X_BED_SIZE 110
+#define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1125,7 +1125,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 160
+#define Z_MAX_POS 120
 
 /**
  * Software Endstops
